3f7fd2ade345c8be142518d442079b4336a74758
João Araújo <j.henr@outlook.com>
2024-11-29 02:00:36 -0300

    feature: block creation of more than one first segment per story

--- alexa-rpg-backoffice/src/domain/usecases/segment/create/create.usecase.ts
+++ alexa-rpg-backoffice/src/domain/usecases/segment/create/create.usecase.ts
@@ -1,6 +1,6 @@
 import { TCreateSegmentInput, TSegmentModel } from '@src/domain/models';
 import { Entities } from '@src/enums';
-import { EntityNotFoundError } from '@src/errors';
+import { EntityAlreadyExistsError, EntityNotFoundError } from '@src/errors';
 import { SegmentRepositoryInterface, StoryRepositoryInterface } from '@src/infra/db/repositories';
 import { TYPES } from '@src/utils/inversify-types';
 import { provideSingleton } from '@src/utils/provide-singleton';
@@ -18,9 +18,17 @@ export class CreateSegmentUseCase implements ICreateSegmentUseCase {
   ) {}
 
   async execute(input: TCreateSegmentInput): Promise<TSegmentModel> {
-    const story = await this.storyRepository.selectOne({ id: input.idStory });
+    const [story, alreadyHasFirstSegment] = await Promise.all([
+      this.storyRepository.selectOne({ id: input.idStory }),
+      this.segmentRepository.selectOne({ idStory: input.idStory, isFirst: true }),
+    ]);
 
     if (!story) throw new EntityNotFoundError(Entities.STORY);
+
+    if (alreadyHasFirstSegment)
+      throw new EntityAlreadyExistsError(
+        `${Entities.STORY} already has a first segment. You can create it as a non first segment and update it later`,
+      );
 
     const segment = await this.segmentRepository.create(input);
 
