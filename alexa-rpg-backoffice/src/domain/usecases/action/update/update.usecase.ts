8b75030181b554ef6577ca932dde13c6a11f7998
João Araújo <j.henr@outlook.com>
2024-12-19 16:35:37 -0300

    refactor: different story validation on action update

--- alexa-rpg-backoffice/src/domain/usecases/action/update/update.usecase.ts
+++ alexa-rpg-backoffice/src/domain/usecases/action/update/update.usecase.ts
@@ -1,7 +1,12 @@
+import { getEnv } from '@src/constants';
 import { TActionModel, TTagModel, TUpdateActionInput } from '@src/domain/models';
 import { Entities, TagTypes } from '@src/enums';
-import { EntityNotFoundError } from '@src/errors';
-import { ActionRepositoryInterface, TagRepositoryInterface } from '@src/infra/db/repositories';
+import { EntityNotFoundError, InvalidParamError } from '@src/errors';
+import {
+  ActionRepositoryInterface,
+  SegmentRepositoryInterface,
+  TagRepositoryInterface,
+} from '@src/infra/db/repositories';
 import { TYPES } from '@src/utils/inversify-types';
 import { provideSingleton } from '@src/utils/provide-singleton';
 import { inject } from 'inversify';
@@ -16,12 +21,22 @@ export class UpdateActionUseCase implements IUpdateActionUseCase {
     private readonly actionRepository: ActionRepositoryInterface,
     @inject(TYPES.repositories.TagRepository)
     private readonly tagRepository: TagRepositoryInterface,
+    @inject(TYPES.repositories.SegmentRepository)
+    private readonly segmentRepository: SegmentRepositoryInterface,
   ) {}
 
   async execute(idAction: string, input: TUpdateActionInput): Promise<TActionModel | null> {
     const oldAction = await this.actionRepository.selectOne({ id: idAction });
 
     if (!oldAction) throw new EntityNotFoundError(Entities.ACTION);
+
+    const [originSegmentExists] = await Promise.all([
+      this.segmentRepository.selectOne({ id: input.idOriginSegment, idStory: oldAction.idStory }),
+      this.validateActionsLimit(oldAction.idStory, input.idOriginSegment),
+      this.validateNextSegments(oldAction.idStory, input),
+    ]);
+
+    if (!originSegmentExists) throw new EntityNotFoundError(`${Entities} origin`);
 
     const tagNamesToCreate = await this.tagRepository.checkNonExistentTags(oldAction.idStory, input.tags, 'action');
     const tagsToCreate = tagNamesToCreate.map((name): Partial<TTagModel> => {
@@ -38,5 +53,29 @@ export class UpdateActionUseCase implements IUpdateActionUseCase {
     ]);
 
     return response;
+  }
+
+  private async validateNextSegments(idStory: string, input: TUpdateActionInput): Promise<void> {
+    const nextSegmentsIds = Array.from(
+      new Set([input.idSegmentSuccess, ...(input.idSegmentFailure ? [input.idSegmentFailure] : [])]),
+    );
+
+    const nextSegments = await this.segmentRepository.selectMany({ id: In(nextSegmentsIds), idStory });
+
+    if (nextSegments.length !== nextSegmentsIds.length) {
+      const foundSegmentsIds = nextSegments.map((segment) => segment.id);
+
+      const idsNotFound = nextSegmentsIds.filter((uniqueId) => !foundSegmentsIds.includes(uniqueId));
+
+      throw new InvalidParamError(`Some segment relations were not found: ${idsNotFound}`);
+    }
+  }
+
+  private async validateActionsLimit(idStory: string, idOriginSegment: string): Promise<void> {
+    const actionsCount = await this.actionRepository.count({ idOriginSegment, idStory });
+
+    if (actionsCount >= getEnv().actionLimit) {
+      throw new InvalidParamError(`Request exceeds the allowed limit of ${getEnv().actionLimit} actions per segment.`);
+    }
   }
 }
