47339f928e295b82ce9d40b8920af67f2a2ed526
João Araújo <j.henr@outlook.com>
2024-12-20 16:02:13 -0300

    refactor: remove unusd file

--- alexa-rpg-backoffice/test.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-
-function generateSHA256Hash(password) {
-  return crypto.createHash("sha256").update(password).digest("hex");
-}
-
-const password = "alexaRPG";
-const hashedPassword = generateSHA256Hash(password);
-console.log("SHA-256 Hashed Password:", hashedPassword);
