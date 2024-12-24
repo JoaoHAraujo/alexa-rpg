6a544e20e18c1bfcdae18e9eb46780bbccf46b23
João Araújo <j.henr@outlook.com>
2024-12-24 17:16:36 -0300

    refactor: auth imports

--- alexa-rpg-backoffice/src/controllers/v1/auth.controller.ts
+++ alexa-rpg-backoffice/src/controllers/v1/auth.controller.ts
@@ -1,9 +1,10 @@
-import { ILoginUseCase, TLoginInput } from '@src/domain/usecases';
 import { TYPES } from '@src/utils/inversify-types';
 import { provideSingleton } from '@src/utils/provide-singleton';
 import { inject } from 'inversify';
 import { BaseHttpController, interfaces } from 'inversify-express-utils';
 import { Body, Post, Route, Tags } from 'tsoa';
+
+import { ILoginUseCase, TLoginInput } from '../../domain/usecases';
 
 @Route('v1/auth')
 @Tags('Auth')
