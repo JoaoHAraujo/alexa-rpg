e067bf3f2064d5d111961e345a37682adbd7fa10
João Araújo <j.henr@outlook.com>
2024-12-24 17:15:53 -0300

    feature: login

--- alexa-rpg-backoffice/src/controllers/_index.ts
+++ alexa-rpg-backoffice/src/controllers/_index.ts
@@ -1,6 +1,7 @@
 export * from './_config';
 export * from './v1/action.controller';
 export * from './v1/admin.controller';
+export * from './v1/auth.controller';
 export * from './v1/health.controller';
 export * from './v1/segment.controller';
 export * from './v1/story.controller';
