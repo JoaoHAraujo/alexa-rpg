261a50a41b5b5afbf0e15e8b0fdbd51ae97746fe
João Araújo <j.henr@outlook.com>
2025-03-18 22:45:46 -0300

    refactor: skill name and presentation

--- alexa-rpg-lambda/src/handlers/base-handlers/launch-request.handler.ts
+++ alexa-rpg-lambda/src/handlers/base-handlers/launch-request.handler.ts
@@ -10,7 +10,7 @@ export const LaunchRequestHandler: RequestHandler = {
   },
   async handle(handlerInput: HandlerInput): Promise<Response> {
     try {
-      const speechText = 'Bem-vindo à primeira versão da skill "Decida e Viva". ';
+      const speechText = 'Bem-vindo à primeira versão da skill "Voz e Ação". ';
       const askBirthDateText = 'Para começar, informe sua data de nascimento';
 
       const responseBuilder = handlerInput.responseBuilder
